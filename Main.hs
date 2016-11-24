f9bfad4480976cf0dcf25359036cfb69f5416537
paolino <paolo.veronelli@gmail.com>
2016-11-24 20:10:40 +0100

    faster state, refactored search to stay in the tree

--- Main.hs
+++ Main.hs
@@ -1,54 +1,34 @@
+{-# language ViewPatterns, ImplicitParams #-}
 import Data.Char
 import qualified Data.Map as M
-import Data.List (foldl', sort,intercalate )
+import Data.Map (Map)
+import Data.List (foldl', sort,intercalate,group)
 import System.IO
 import Control.Monad
-import Control.Arrow (second)
+import Control.Arrow (second, (&&&))
 
 import Trie
 
-consume xs = let
-  ys = sort $ xs
-  skip ys x [] = []
-  skip ys z (x:xs)  | z == x = skip (x:ys) z xs
-                    | otherwise = consume' ys (x:xs)
-  consume' ys [x] = [(x,ys)]
-  consume' ys (x:xs) = (x,ys ++ xs) : skip (x:ys) x xs
-  in consume' [] xs
+type Occ a = Map a Int
 
+mkOcc :: Ord a => [a] -> Map a Int
+mkOcc = M.fromList . map (head &&& length) . group . sort
 
-search :: Ord a => Trie a -> [a] -> [a] -> [([a],[a])]
-search L _ xs = return $ (xs,[])
-search (Trie False m) _ [] = []
-search (Trie True m) _ [] = return ([],[])
-search (Trie b m) zs xs = (if b then ((xs,[]):) else id) $ do
-  (x,ys) <- consume xs
-  let (c,zs') = case zs of
-                  [] -> (True,[])
-                  (z:zs) -> case compare x z of
-                              EQ -> (True,zs)
-                              GT -> (True,[])
-                              LT -> (False,undefined)
+consume :: Ord a => Occ a -> [(a,Occ a)]
+consume m = let
+  f m k = M.update (\n -> if n > 1 then Just (n - 1) else Nothing) k m
+  in map (id &&& f m) $ M.keys m
 
-  if c then
-    case x `M.lookup` m of
-      Nothing -> []
-      Just t -> map (second (x:)) $ search t zs' ys
-    else []
-
-deepsearch :: Ord a => Trie a -> [a] -> [a] -> [[[a]]]
-deepsearch t _ [] = []
-deepsearch t zs xs = do
-  (rs,ys) <- search t zs xs
-  case rs of
-    [] -> return [ys]
-    rs -> map (ys:) $ deepsearch t ys rs
 
 english = do
   ls <- map (map toLower) <$> lines <$> readFile "words.txt"
   return $ foldl' (flip insert) L $ ls
 
 searchI t = do
-  forever $ putStr "> " >> hFlush stdout >> getLine >>= mapM putStrLn . (map $ intercalate " ") . deepsearch t [] . sort . map toLower . filter isLetter
+  let ?conf = Search M.null consume
+  forever $ do
+      putStr "> " >> hFlush stdout
+      l <- mkOcc <$> map toLower <$> filter isLetter <$> getLine
+      mapM putStrLn . (map $ intercalate " ") . completeSearch t [] $ l
 
 main =  english >>= searchI
--- Trie.hs
+++ Trie.hs
@@ -1,21 +1,77 @@
+{-# language ViewPatterns, ImplicitParams #-}
 module Trie where
 
 import Data.Map (Map)
 import qualified Data.Map as M
-import Data.List (foldl', tails,sort,intercalate )
+import Data.List ()
+import Control.Arrow (second)
 
 
+-- | A trie holds information in the indexing of children
+-- The Bool value indicate a node is also terminal
 data Trie a = Trie Bool (Map a (Trie a)) | L deriving Show
 
+-- insertion --------------------
+--
+-- | insert a path
 insert :: Ord a => [a] -> Trie a -> Trie a
 insert [] L = L
-insert [] (Trie _ m) = Trie True m
+insert [] (Trie _ m) = Trie True m -- set the node as terminal
 insert (x:xs) L = Trie False $ M.singleton x $ insert xs L
 insert (x:xs) (Trie b m) = Trie b $ M.insertWith merge x (insert xs L) m
 
+-- | merge 2 tries
 merge L L = L
-merge (Trie _ m) L = Trie True m
+merge (Trie _ m) L = Trie True m -- one was terminal (L)
 merge L (Trie _ m) = Trie True m
 merge (Trie b m) (Trie b' m') = Trie (b || b') $ M.unionWith merge m' m
 
+
+-- searching library ----------------------
+--
+--
+-- | a configuration for a search , 'm' is the search state 'a' is the indexing in the trie
+data Search m a = Search {
+  empty :: m -> Bool, -- check if the search is over
+  expand :: m -> [(a,m)] -- produce choices from a search state
+                         }
+
+-- | search through a trie until a leaf is reached.
+search  :: (Ord a,?conf :: Search m a)
+
+        => Trie a -- ^ trie
+        -> [a] -- ^ lower bound (lexicographically) for the paths
+        -> m -- ^ initial search state
+        -> [(m,[a])] -- all found leaves
+search L _ xs = return $ (xs,[])
+search (Trie b m) _ h@(empty ?conf -> True)  = if b then return (h,[]) else []
+search (Trie b m) zs h = (if b then ((h,[]):) else id) $ do
+
+  (x,ys) <- expand ?conf h -- next steps
+
+  let (c,zs') = -- matching lexicographically successor
+        case zs of
+          [] ->  (True,[]) -- no problem we are lessicographically higher
+          (z:zs) -> case compare x z of
+                      EQ -> (True,zs) -- we are matching, check on next level
+                      GT -> (True,[]) -- we are higher from now on
+                      LT -> (False,undefined) -- we are lower, don't go on
+
+  if c then
+    case x `M.lookup` m of
+      Nothing -> []
+      Just t -> map (second (x:)) $ search t zs' ys
+    else []
+
+completeSearch  :: (Ord a, ?conf :: Search m a)
+                => Trie a
+                -> [a]
+                -> m
+                -> [[[a]]]
+completeSearch t _ ( empty ?conf -> True) = []
+completeSearch t zs xs = do
+  (rs,ys) <- search t zs xs
+  case rs of
+    (empty ?conf -> True) -> return [ys]
+    rs -> map (ys:) $ completeSearch t ys rs
 
